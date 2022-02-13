748fa37346dec486f01f48b80d3ed58f8c2db099
Phil Curran <pecurran@hotmail.com>
2022-02-12 21:06:05 -0800

    added all similar tests to PastryTests.cs from BreadTests.cs.

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -31,5 +31,62 @@ namespace Bakery.Models
       Assert.AreEqual(newPastryOrder.GetTotal(1), 2);
     }
 
+    [TestMethod]
+    public void GetTotalForTwoBreads()
+    {
+      Bread newBreadOrder = new Bread();
+      Assert.AreEqual(newBreadOrder.GetTotal(2), 10);
+    }
+
+    [TestMethod]
+    public void GetTotalForThreeBreads()
+    {
+      Bread newBreadOrder = new Bread();
+      Assert.AreEqual(newBreadOrder.GetTotal(3), 10);
+    }
+
+    [TestMethod]
+    public void GetTotalForFourBreads()
+    {
+      Bread newBreadOrder = new Bread();
+      Assert.AreEqual(newBreadOrder.GetTotal(4), 15);
+    }
+
+    [TestMethod]
+    public void GetTotalForFiveBreads()
+    {
+      Bread newBreadOrder = new Bread();
+      Assert.AreEqual(newBreadOrder.GetTotal(5), 20);
+    }
+
+    [TestMethod]
+    public void GetTotalForSixBreads()
+    {
+      Bread newBreadOrder = new Bread();
+      Assert.AreEqual(newBreadOrder.GetTotal(6), 20);
+    }
+
+    [TestMethod]
+    public void GetTotalForSevenBreads()
+    {
+      Bread newBreadOrder = new Bread();
+      Assert.AreEqual(newBreadOrder.GetTotal(7), 25);
+    }
+
+    [TestMethod]
+    public void GetTotalForOneMillionBreads()
+    {
+      Bread newBreadOrder = new Bread();
+      Assert.AreEqual(newBreadOrder.GetTotal(1000000), 3333335);
+    }
+
+    [TestMethod]
+    public void GetTotalForZEROBreads()
+    {
+      Bread newBreadOrder = new Bread();
+      Assert.AreEqual(newBreadOrder.GetTotal(0), 0);
+    }
+
+
   }
 }
