7815695f51689da8b31e45a1d969558a0e4b244f
Phil Curran <pecurran@hotmail.com>
2022-02-12 21:15:09 -0800

    added Program.cs file and boilerplate from lesson for reference.

---
+++ Bakery/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+namespace Bakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Enter maximum price: ");
+      // string stringMaxPrice = Console.ReadLine();
+      // int maxPrice = int.Parse(stringMaxPrice);
+
+      // List<Car> CarsMatchingSearch = new List<Car>(0);
+
+      // foreach (Car automobile in Cars)
+      // {
+      //   if (automobile.WorthBuying(maxPrice))
+      //   {
+      //     CarsMatchingSearch.Add(automobile);
+      //   }
+      // }
+
+      // foreach(Car automobile in CarsMatchingSearch)
+      // {
+      //   Console.WriteLine(automobile.MakeModel);
+      // }
+    }
+  }
+}
