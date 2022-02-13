0fced44a39c5d192a5f86a9b77fb4e15add02ccd
Phil Curran <pecurran@hotmail.com>
2022-02-12 16:55:26 -0800

    added passing test for seven breads.

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -8,7 +8,18 @@ namespace Bakery.Models
 
     public int GetTotal(int numberOrdered)
     {
-      int total = numberOrdered * 5;
+      int price = 5;
+      int total = 0;
+      if (numberOrdered % 3 == 0)
+        {
+          total = (numberOrdered / 3 * 2) * price;
+        }
+        else if (numberOrdered % 3 < 3)
+        {
+          int diff = numberOrdered % 3;
+          int temp = numberOrdered - diff;
+          total = (temp / 3 * 2) * price + diff * price;
+        }
       return total;
     }
 
